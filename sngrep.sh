ac57e6fce45135f06e9f0433cd335d1fc0373a4b
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2024-11-30 08:57:08 +0700

    Update sngrep.sh

--- sngrep.sh
+++ sngrep.sh
@@ -1,11 +1,11 @@
 apt remove sngrep -y
 cd /tmp
-wget http://deb.debian.org/debian/pool/main/s/sngrep/sngrep_1.4.8.orig.tar.gz
-tar -xzvf sngrep_1.4.8.orig.tar.gz
+wget https://ftp.debian.org/debian/pool/main/s/sngrep/sngrep_1.8.2.orig.tar.gz
+tar -xzvf sngrep_1.8.2.orig.tar.gz
 sudo apt install -y build-essential libpcap-dev libncurses5-dev libssl-dev
-cd sngrep-1.4.8
+cd /tmp/sngrep-1.8.2
 ./bootstrap.sh
 ./configure
 make
 make install
-cp /tmp/sngrep-1.4.8/src/sngrep /usr/bin/
+cp /tmp/sngrep-1.8.2/src/sngrep /usr/bin/
